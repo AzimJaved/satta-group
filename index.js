45cf0cc444fa82463e17c54614c8de8978ff6a51
Azim Javed <thatazimjaved@gmail.com>
2019-06-28 12:27:14 +0530

    Progress

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const scraper = require('./util/scraper.js')
 const seriesId = 8089; // World cup
-
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 scraper.fetchHtml().then( (html) => {
     var scoreboard = scraper.parseHtml(html)
     console.log(scoreboard)
 })
 
+
+app.listen(PORT, () => {
+    console.log("Booted")
+})
 
